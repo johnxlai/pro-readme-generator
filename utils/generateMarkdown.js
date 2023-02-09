f0e4fed501ee1975ea2666517d935489fc48c4c7
John Lai <lai.john.py@gmail.com>
2023-02-08 23:03:34 -0500

    Update generate markdown js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,9 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  console.log(license);
+  return `
+  ![License Badge](https://img.shields.io/badge/license-${license}-brightgreen)
+  `;
 }
 
 // TODO: Create a function that returns the license link
@@ -19,11 +21,20 @@ function renderLicenseSection(licenseSection) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   console.log(data);
-  const { title, projectDesc, gitHubUserName, email, tests } = data;
-  var badge = renderLicenseBadge(data.license);
+  const {
+    title,
+    projectDesc,
+    installation,
+    usage,
+    license,
+    contributing,
+    tests,
+    gitHubUserName,
+    email,
+  } = data;
+  const badge = renderLicenseBadge(license);
   return `
 # ${title}
-liscense
 
 ##${badge}
 
@@ -37,25 +48,18 @@ liscense
 - [How to Contribute](#how-to-contribute)
 - [Questions](#questions)
 
-WHEN I click on the links in the Table of Contents
-THEN I am taken to the corresponding section of the README
-
 ## Installation
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+${installation}
 
 ## Usage
-Provide instructions and examples for use. Include screenshots as needed.
+${usage}
 
 ## License
 The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
 
-## Badges
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
 
 ## How to Contribute
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer
+${contributing}
 
 ## Tests
 ${tests}
